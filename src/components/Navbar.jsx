9dc286d722178ffc9ec1e8da2a83cb750cdade43
Viktor Kovács <67899758+victorsonet@users.noreply.github.com>
2024-03-28 12:37:27 +0100

    Favicon and Navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <nav className="bg-emerald-100">
       <div className="align-element py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8">
         <h2 className="text-3xl font-bold">
-          Web<span className="text-emerald-600">Dev</span>
+          Web<span className="text-emerald-600">Developer</span>
         </h2>
         <div className="flex gap-x-3">
           {links.map(({ id, href, text }) => {
